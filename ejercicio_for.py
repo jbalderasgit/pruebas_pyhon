328b1e69d79d65bc711d08bb70b288cb30f7326e
Jorge Balderas <jorge.balderas.luna@gmail.com>
2020-06-02 20:58:24 -0500

    ejercicios en python

---
+++ ejercicio_for.py
@@ -0,0 +1,29 @@
+
+
+# RETO 1
+print("Reto 1")
+for i in range(0,300,2):
+          print(i)
+
+# RETO 2
+print("Reto 2")
+for i in range(1,20):
+         res=i*15
+         print(res)
+
+#RETO 3
+print("Reto 3")
+res2=0
+for i in range(59,100,2):
+         res2=res2+i
+pass         
+print(res2)
+
+#RETO 4
+print("Reto 4")
+res3=0
+for i in range(10,100,1):
+         if i%2:
+                res3=res3+i
+pass
+print(res3)                
